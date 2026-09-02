b4fd7b6b654c98d58681a8f73955c5879f66f042
Breno Oliveira <oliveira.brn19@gmail.com>
2026-09-02 13:21:09 -0300

    removido repositorio paralelo

--- app/elo/src/util/RemoverTagsHtml.tsx
+++
@@ -1,8 +0,0 @@
-export const removerTagsHtml = (htmlString: string): string => {
-    if (!htmlString) return '';
-
-    return htmlString
-        .replace(/<[^>]+>/g, '') 
-        .replace(/&nbsp;/g, ' ') 
-        .trim();
-};
